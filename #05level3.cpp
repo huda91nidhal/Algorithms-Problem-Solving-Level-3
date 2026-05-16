9921eebc0fb004ca2b168754ccd7af85f5d0c83f
huda91nidhal <hudaalshomar@gmail.com>
2026-05-16 15:30:50 +0300

    Add files via upload

---
+++ #05level3.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+int RandomNumber(int From, int To)
+{
+	int randomnum = rand() % (To - From + 1) + From;
+	return randomnum;
+}
+void FillMatrix3By3WithRandomNumber(int arr[3][3], short row, short col)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] = RandomNumber(1, 100);
+		}
+
+	}
+}
+void PrintMatrex(int arr[3][3], short row, short col)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << setw(3) << arr[i][j] << "  ";
+		}
+		cout << endl;
+
+	}
+}
+int ColSum(int arr[3][3], short Row, short Col)
+{
+	int Sum = 0;
+	for (int i = 0; i <= Row - 1; i++)
+	{
+		Sum += arr[i][Col];
+	}
+	return Sum;
+}
+void SumMatrixColInArray(int arr[3][3], int arrSum[3], short Row, short Col)
+{
+	for (int j = 0; j < Row; j++)
+	{
+		arrSum[j] = ColSum(arr, Row, j);
+	}
+}
+void PrintColSumsArray(int arrSum[3], int i)
+{
+	cout << "The Results Of The Sum is " << endl;
+	for (i = 0; i < 3; i++)
+	{
+		cout << "The Sum Of Col " << i + 1 << " = " << arrSum[i] << endl;
+	}
+}
+
+int main()
+{
+	int arr[3][3]; int arrSum[3];
+	srand((unsigned)time(NULL));
+	FillMatrix3By3WithRandomNumber(arr, 3, 3);
+	cout << "The Random Number of Matrex [3][3] is : " << endl;
+	PrintMatrex(arr, 3, 3);
+	SumMatrixColInArray(arr, arrSum, 3, 3);
+	PrintColSumsArray(arrSum, 3);
+	return 0;
+}
