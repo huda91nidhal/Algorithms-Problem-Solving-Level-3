9b723f5b84d7aa3fc0024049aa177a0e8808c6d3
huda91nidhal <hudaalshomar@gmail.com>
2026-05-16 15:37:59 +0300

    Add files via upload

---
+++ #15level3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf(" %0*d ", 2, arr[i][j]);
+        }
+        cout << "\n";
+    }
+}
+
+int CountNumberInMatrix(int arr[3][3], int Num ,  short Row, short Col)
+{
+    int Counter = 0 ;
+    
+    for (int i = 0; i < Row; i++)
+    {
+        for (int j = 0; j < Col; j++)
+        {
+            if (arr[i][j] == Num)
+            {
+                Counter++;
+            }
+        }
+    }
+    return Counter;
+}
+int main()
+{
+    int arr1[3][3] = { {9,9,0}, {0,1,9}, {9,9,1} };
+    int Num;
+    //int arr1[3][3] = {5,0,0,0,5,0,0,0,5};
+    cout << "\n matrix 1 :\n";
+    PrintMatrix(arr1, 3, 3);
+    cout << "Enter The Number To Count In Matrix : " << endl;
+    cin >> Num;
+    cout << "Number " << Num << " Count in Matrix is " <<CountNumberInMatrix(arr1,Num , 3, 3) << endl;
+    system("pause>0");
+}
