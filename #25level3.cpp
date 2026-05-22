eac35401f59ce443ac47ed99237d085c21f95bda
huda91nidhal <hudaalshomar@gmail.com>
2026-05-22 11:05:16 +0300

    Add files via upload

---
+++ #25level3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cctype>
+using namespace std;
+string ReadString()
+{
+    string Text;
+    cout << "Please Enter Your String : " << endl;
+    getline(cin, Text);
+    return Text;
+}
+string PrintLowerFirstLetterOfEachString(string Text)
+{
+    bool FirstLetter = true;
+    for (short i = 0; i < Text.length(); i++)
+    {
+        if (Text[i] != ' ' && FirstLetter)
+        {
+            Text[i] = tolower(Text[i]);
+        }
+        FirstLetter = (Text[i] == ' ' ? true : false);
+    }
+    return Text;
+}
+int main()
+{
+    string Text;
+    Text = ReadString();
+    Text = PrintLowerFirstLetterOfEachString(Text);
+    cout << Text << endl;
+    system("pause>0");
+}
